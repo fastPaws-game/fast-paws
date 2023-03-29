2174e575521cbc57bfd0ced3b2fb276f95dc8477
Maxim <m.solomahin@icloud.com>
2023-03-29 23:48:37 +0300

    solomahinmaxim/fpg-30: правки

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,12 @@
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { useChangeTheme } from './hooks/useChangeTheme'
 import { useFetchServerData } from './hooks/useFetchServerData'
 import { GlobalStyles } from './assets/styles/globalStyle'
 import { BrowserRouter } from 'react-router-dom'
-import GamePause from './components/gamePause'
-import Button from './ui/button'
-import { useCallback, useState } from 'react'
 
 function App() {
   useFetchServerData()
   const { theme, themeToggler } = useChangeTheme()
-
-  const [modal, setModal] = useState(false)
-
-  const handelCloseModal = useCallback(() => {
-    setModal(false)
-  }, [setModal])
 
   return (
     <BrowserRouter>
@@ -23,8 +14,6 @@ function App() {
         <GlobalStyles />
         <button onClick={themeToggler}>Toggle Theme</button>
         {/*<Router/>*/}
-        <Button onClick={() => setModal(true)} size={'small'}>Open</Button>
-        <GamePause visible={modal} handleClose={handelCloseModal} />
       </ThemeProvider>
     </BrowserRouter>
   )
