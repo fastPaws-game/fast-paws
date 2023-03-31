45f642a3b1f898d253b1872c7c97262a6592865a
mrHoft <ru.hoft@gmail.com>
2023-04-01 02:04:02 +0300

    prettier, avatar

--- packages/client/src/components/ProfileAvatar.tsx
+++ packages/client/src/components/ProfileAvatar.tsx
@@ -1,41 +1,85 @@
-import {createRef, ChangeEvent} from 'react'
-import styled from 'styled-components';
-// import DefaultAvatar from '../assets/icons/DefaultAvatar';
+import { createRef, ChangeEvent } from 'react'
+import styled from 'styled-components'
+import DefaultAvatar from '../assets/icons/DefaultAvatar.svg'
 
-export default function ProfileAvatar(){
-	const fileUpload = createRef<HTMLInputElement>();
+export default function ProfileAvatar() {
+  const fileUpload = createRef<HTMLInputElement>()
+  const image = createRef<HTMLImageElement>()
 
-	function fileChange(event:ChangeEvent<HTMLInputElement>){
-		const target=event.target as HTMLInputElement;
-		const file=target.files? target.files[0] : null;
-		if(file){
-			const data=new FormData();
-			data.append('avatar', file);
-			console.log(file);
-			/* Change profile avatar API
-				alert(JSON.stringify(data));
+  function fileChange(event: ChangeEvent<HTMLInputElement>) {
+    const target = event.target as HTMLInputElement
+    const file = target.files ? target.files[0] : null
+    if (file) {
+      image.current!.src = URL.createObjectURL(file)
+      const data = new FormData()
+      data.append('avatar', file)
+      /* Change profile avatar API
+      console.log(file)
+			alert(JSON.stringify(data));
 			*/
-		}
-	}
+    }
+  }
 
-	const fileChoose=()=>{
-		const node=fileUpload.current;
-		if(node) node.click();
-	}
+  function fileChoose() {
+    const node = fileUpload.current
+    if (node) {
+      node.click()
+    }
+  }
 
-	return (
-	<Avatar onClick={fileChoose}>
-		<input type="file" ref={fileUpload} onChange={fileChange} name="file" hidden accept="image/png, image/jpeg, image/gif"/>
-	</Avatar>
-	);
+  return (
+    <Avatar onClick={fileChoose}>
+      <label htmlFor={'file'}>
+        <span>Change</span>
+      </label>
+      <input
+        type="file"
+        ref={fileUpload}
+        onChange={fileChange}
+        name="file"
+        hidden
+        accept="image/png, image/jpeg, image/gif"
+      />
+      <img src={DefaultAvatar} ref={image} />
+    </Avatar>
+  )
 }
 
-const Avatar=styled.div`
-	width: 120px;
-	height: 120px;
-	border-radius: 50%;
-	background-color: ${props=>props.theme.colors.accent};
-	background-image: url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 256'%3E%3Crect width='256' height='256' fill='none'/%3E%3Cline x1='128' y1='192' x2='128' y2='224' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3Ccircle cx='84' cy='140' r='8'/%3E%3Ccircle cx='172' cy='140' r='8'/%3E%3Cline x1='128' y1='48' x2='128' y2='88' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3Cpolyline points='144 176 128 192 112 176' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3Cline x1='96' y1='53' x2='96' y2='88' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3Cline x1='160' y1='53' x2='160' y2='88' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3Cpath d='M32,136V51.3a8,8,0,0,1,13.7-5.6L67.6,67.6h0A100.8,100.8,0,0,1,128,48a100.8,100.8,0,0,1,60.4,19.6h0l21.9-21.9A8,8,0,0,1,224,51.3V136c0,48.6-43,88-96,88S32,184.6,32,136Z' fill='none' stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='8'/%3E%3C/svg%3E");
-	background-size: 120px;
-	cursor: pointer;
-`;
+const Avatar = styled.div`
+  color: transparent;
+  transition: all 0.3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  transition: all 0.3s ease;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: ${props => props.theme.colors.accent};
+  box-shadow: ${({ theme }) => theme.shadows.secondary};
+  cursor: pointer;
+  &:hover {
+    label {
+      width: 120px;
+      height: 120px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 10000;
+      color: rgb(250, 250, 250);
+      transition: background-color 0.2s ease-in-out;
+      border-radius: 100px;
+      margin-bottom: 0;
+      cursor: pointer;
+    }
+  }
+  img {
+    position: absolute;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    z-index: 0;
+  }
+`
--- packages/client/src/pages/ProfilePage.tsx
+++ packages/client/src/pages/ProfilePage.tsx
@@ -1,53 +1,57 @@
-import Button from '../ui/button';
-import styled from 'styled-components';
-import ProfileAvatar from '../components/ProfileAvatar';
-import BaseLayout from '../layouts/BaseLayout';
-import { media } from '../assets/styles/media';
-import { useNavigate } from 'react-router';
-import ProfileForm from '../components/ProfileForm';
+import Button from '../ui/button'
+import styled from 'styled-components'
+import ProfileAvatar from '../components/ProfileAvatar'
+import BaseLayout from '../layouts/BaseLayout'
+import { media } from '../assets/styles/media'
+import { useNavigate } from 'react-router'
+import ProfileForm from '../components/ProfileForm'
 
 const ProfilePage = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate()
 
-	const handleClick = (path?: string) => () => {
-		if (path) navigate(path);
-	}
+  const handleClick = (path?: string) => () => {
+    if (path) navigate(path)
+  }
 
-	return (
-		<BaseLayout>
-			<ButtonBack icon={<h1>&lt;</h1>} size={'small'} onClick={handleClick('/')}/>
-			<Profile>
-				<Settings>
-					<ProfileAvatar/>
-					{/* <div><input type={'checkbox'} onClick={themeToggler}/><label> Dark theme</label></div> ToDo: Theme check state */}
-				</Settings>
-				<ProfileForm/>
-			</Profile>
-		</BaseLayout>
-	);
+  return (
+    <BaseLayout>
+      <ButtonBack
+        icon={<h1>&lt;</h1>}
+        size={'small'}
+        onClick={handleClick('/')}
+      />
+      <Profile>
+        <Settings>
+          <ProfileAvatar />
+          {/* <div><input type={'checkbox'} onClick={themeToggler}/><label> Dark theme</label></div> ToDo: Theme check state */}
+        </Settings>
+        <ProfileForm />
+      </Profile>
+    </BaseLayout>
+  )
 }
 
-const ButtonBack=styled(Button)`
-	position: absolute;
-	left: 2em;
-	top: 2em;
-`;
+const ButtonBack = styled(Button)`
+  position: absolute;
+  left: 2em;
+  top: 2em;
+`
 
-const Settings=styled.div`
-	width: 120px;
-	display: flex;
-	flex-direction: column;
-	gap: 1em;
-	margin: 2em;
-`;
+const Settings = styled.div`
+  width: 120px;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  margin: 2em;
+`
 
-const Profile=styled.div`
-	display: flex;
-	justify-content: center;
-	${media.small} {
-		flex-direction: column;
-		align-items: center;
-	}
-`;
+const Profile = styled.div`
+  display: flex;
+  justify-content: center;
+  ${media.small} {
+    flex-direction: column;
+    align-items: center;
+  }
+`
 
-export default ProfilePage;
+export default ProfilePage
