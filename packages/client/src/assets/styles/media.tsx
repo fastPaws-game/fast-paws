f94c10ceeba3a3ff2f1b864e7fba0e22d39fa4c3
Maxim <m.solomahin@icloud.com>
2023-03-31 20:02:00 +0300

    вернул @media

--- packages/client/src/assets/styles/media.tsx
+++ packages/client/src/assets/styles/media.tsx
@@ -1,16 +1,18 @@
 type Breakpoints<T = number> = {
   large?: T,
+  middle?: T,
   small?: T,
 }
 
 export const breakpoints: Breakpoints = {
   large: 1050,
-  small: 460
+  middle: 800,
+  small: 481
 }
 
 export const media = Object.entries(breakpoints).reduce<Breakpoints<string>>(
   (acc, [key, value]) => {
-    acc[key as keyof Breakpoints] = `@media screen and (max-height: ${value}px)`
+    acc[key as keyof Breakpoints] = `@media screen and (max-width: ${value}px)`
     return acc
   },
   {}
