8cf36d216c61cb62b919bb2e1d3fb2ef123b8ac7
Maxim <m.solomahin@icloud.com>
2023-04-13 00:22:22 +0300

    добавил редиректы

--- packages/client/src/hooks/useRedirect.tsx
+++ packages/client/src/hooks/useRedirect.tsx
@@ -5,14 +5,22 @@ import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 
 const pathsToNeedAuth = [routes.MAIN, routes.GAME, routes.FORUM, routes.LEADERBOARD, routes.SETTINGS]
+const pathForAuth = [routes.SIGNUP, routes.HOME]
+
 export const useRedirect = () => {
   const location = useLocation()
   const navigate = useNavigate()
   const isAuth = useAppSelector((store) => store.auth.isAuth)
 
   useEffect(() => {
-    if (pathsToNeedAuth.includes(location.pathname as routes) && !isAuth) {
+    const isAuthRequirePage = pathsToNeedAuth.includes(location.pathname as routes)
+    const isAuthPages = pathForAuth.includes(location.pathname as routes)
+
+    if (isAuthRequirePage && !isAuth) {
       navigate(routes.HOME)
+    }
+    if (isAuthPages && isAuth) {
+      navigate(routes.MAIN)
     }
   }, [isAuth, location.pathname, navigate])
 
