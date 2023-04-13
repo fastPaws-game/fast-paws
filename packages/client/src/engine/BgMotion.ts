904959084656c6fb63d78e36b27033a1373d79b0
ElPastel <gromova.victoria9@gmail.com>
2023-04-13 15:00:36 +0400

    format

--- packages/client/src/engine/BgMotion.ts
+++ packages/client/src/engine/BgMotion.ts
@@ -2,95 +2,95 @@ import canvas from '../constants/canvas'
 import ResourceLoader from './ResourceLoader'
 
 export type Layer = {
-    img: HTMLImageElement | null
-    x: number
-    y: number
-    dx: number
-    imgW: number
-    imgH: number
+  img: HTMLImageElement | null
+  x: number
+  y: number
+  dx: number
+  imgW: number
+  imgH: number
 }
 
 type LayersData = {
-    src: string
-    dx: number
-    isTop?: boolean
+  src: string
+  dx: number
+  isTop?: boolean
 }
 
 const layersData: LayersData[] = [
-    { src: 'layer1', dx: 0, isTop: true },
-    { src: 'layer2', dx: -0.2 },
-    { src: 'layer3', dx: -0.8 },
+  { src: 'layer1', dx: 0, isTop: true },
+  { src: 'layer2', dx: -0.2 },
+  { src: 'layer3', dx: -0.8 },
 ]
 
 export default class BgMotion {
-    private ctx: CanvasRenderingContext2D | null = null
-    private timer: NodeJS.Timeout | null = null
-    private layersArr: Layer[] = []
-    private clearX = canvas.width
-    private clearY = canvas.height
-    private static __instance: BgMotion
+  private ctx: CanvasRenderingContext2D | null = null
+  private timer: NodeJS.Timeout | null = null
+  private layersArr: Layer[] = []
+  private clearX = canvas.width
+  private clearY = canvas.height
+  private static __instance: BgMotion
 
-    constructor(ctx: CanvasRenderingContext2D) {
-        if (BgMotion.__instance) {
-            if (!ctx) return
-            return BgMotion.__instance
-        }
-
-        this.ctx = ctx
-        layersData.forEach(layer => {
-            const layerObj: Layer = {
-                img: null,
-                x: 0,
-                y: 0,
-                dx: 0,
-                imgW: 0,
-                imgH: 0,
-            }
-            const img = ResourceLoader.sprite[layer.src] as HTMLImageElement
-            layerObj.img = img
-            layerObj.x = 0
-            layerObj.dx = layer.dx
-            const aspectRatio = img.height / img.width
-            layerObj.imgW = canvas.width
-            layerObj.imgH = layerObj.imgW * aspectRatio
-            layer.isTop ? (layerObj.y = 0) : (layerObj.y = canvas.height - layerObj.imgH)
-            this.layersArr.push(layerObj)
-
-        })
-
-        BgMotion.__instance = this
-
+  constructor(ctx: CanvasRenderingContext2D) {
+    if (BgMotion.__instance) {
+      if (!ctx) return
+      return BgMotion.__instance
     }
 
-    public draw() {
-        this.ctx?.clearRect(0, 0, this.clearX, this.clearY)
-        this.layersArr.forEach(layer => {
-            if (layer.x <= -canvas.width) layer.x = 0
-            if (layer.x > -canvas.width)
-                this.ctx?.drawImage(
-                    layer.img as CanvasImageSource,
-                    canvas.width + layer.x,
-                    layer.y,
-                    layer.imgW,
-                    layer.imgH
-                )
+    this.ctx = ctx
+    layersData.forEach(layer => {
+      const layerObj: Layer = {
+        img: null,
+        x: 0,
+        y: 0,
+        dx: 0,
+        imgW: 0,
+        imgH: 0,
+      }
+      const img = ResourceLoader.sprite[layer.src] as HTMLImageElement
+      layerObj.img = img
+      layerObj.x = 0
+      layerObj.dx = layer.dx
+      const aspectRatio = img.height / img.width
+      layerObj.imgW = canvas.width
+      layerObj.imgH = layerObj.imgW * aspectRatio
+      layer.isTop
+        ? (layerObj.y = 0)
+        : (layerObj.y = canvas.height - layerObj.imgH)
+      this.layersArr.push(layerObj)
+    })
 
-            this.ctx?.drawImage(
-                layer.img as CanvasImageSource,
-                layer.x,
-                layer.y,
-                layer.imgW,
-                layer.imgH
-            )
-            layer.x += layer.dx
-        })
-    }
+    BgMotion.__instance = this
+  }
 
-    public start(speed = 20) {
-        if (!this.timer) this.timer = setInterval(this.draw.bind(this), speed)
-    }
+  public draw() {
+    this.ctx?.clearRect(0, 0, this.clearX, this.clearY)
+    this.layersArr.forEach(layer => {
+      if (layer.x <= -canvas.width) layer.x = 0
+      if (layer.x > -canvas.width)
+        this.ctx?.drawImage(
+          layer.img as CanvasImageSource,
+          canvas.width + layer.x,
+          layer.y,
+          layer.imgW,
+          layer.imgH
+        )
 
-    public stop() {
-        if (this.timer) clearInterval(this.timer)
-    }
+      this.ctx?.drawImage(
+        layer.img as CanvasImageSource,
+        layer.x,
+        layer.y,
+        layer.imgW,
+        layer.imgH
+      )
+      layer.x += layer.dx
+    })
+  }
+
+  public start(speed = 20) {
+    if (!this.timer) this.timer = setInterval(this.draw.bind(this), speed)
+  }
+
+  public stop() {
+    if (this.timer) clearInterval(this.timer)
+  }
 }
