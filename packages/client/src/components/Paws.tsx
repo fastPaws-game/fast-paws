84d64955e721ba5ceca1d83933ab45578ea75b2e
ElPastel <gromova.victoria9@gmail.com>
2023-04-18 11:32:49 +0400

    change paw picture

--- packages/client/src/components/Paws.tsx
+++ packages/client/src/components/Paws.tsx
@@ -1,64 +1,34 @@
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 import { FC } from 'react'
+import paw from '../assets/icons/paw.svg'
+import { media } from '../assets/styles/media'
 
 const Paws: FC = () => {
   return (
     <>
       <Paw1>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw1>
       <Paw2>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw2>
       <Paw3>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw3>
       <Paw4>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw4>
       <Paw5>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw5>
       <Paw6>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw6>
       <Paw7>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw7>
       <Paw8>
-        <PadLarge />
-        <PadSmall1 />
-        <PadSmall2 />
-        <PadSmall3 />
-        <PadSmall4 />
+        <Icon />
       </Paw8>
     </>
   )
@@ -69,138 +39,127 @@ const walk = keyframes`
   100% {opacity: 0;}
 `
 
-const PadLarge = styled.div`
-  background-color: ${props => props.theme.colors.secondary};
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  display: block;
+const basePaw = css`
+  opacity: 0;
   position: absolute;
-  width: 70px;
-  height: 80px;
-  transform: rotate(80deg);
-  left: 100px;
-  top: 50px;
 `
 
-const PadSmall1 = styled.div`
-  width: 25px;
-  height: 27px;
+const Icon = styled.div`
+  mask-image: url(${paw});
+  height: 135px;
+  width: 130px;
   background-color: ${props => props.theme.colors.secondary};
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  display: block;
-  position: absolute;
-  transform: rotate(50deg);
-  left: 145px;
-  top: 28px;
-`
-
-const PadSmall2 = styled.div`
-  width: 25px;
-  height: 27px;
-  background-color: ${props => props.theme.colors.secondary};
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  display: block;
-  position: absolute;
-  transform: rotate(65deg);
-  left: 174px;
-  top: 50px;
-`
-
-const PadSmall3 = styled.div`
-  width: 25px;
-  height: 27px;
-  background-color: ${props => props.theme.colors.secondary};
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  display: block;
-  position: absolute;
-  transform: rotate(98deg);
-  position: absolute;
-  left: 178px;
-  top: 87px;
-`
-
-const PadSmall4 = styled.div`
-  width: 25px;
-  height: 27px;
-  background-color: ${props => props.theme.colors.secondary};
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  display: block;
-  position: absolute;
-  transform: rotate(140deg);
-  position: absolute;
-  left: 158px;
-  top: 117px;
 `
 
 const Paw1 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 75px;
-  top: 300px;
-  transform: rotate(-40deg);
+  top: 250px;
   animation: ${walk} 3s linear infinite;
+
+  ${media.small} {
+    display: none;
+  }
+
+  ${media.middle} {
+    left: 50px;
+    top: 200px;
+  }
 `
 
 const Paw2 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 275px;
   top: 280px;
   transform: rotate(-5deg);
   animation: ${walk} 3s linear infinite 0.25s;
+
+  ${media.small} {
+    display: none;
+  }
+
+  ${media.middle} {
+    left: 150px;
+    top: 40px;
+  }
 `
 
 const Paw3 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 375px;
   top: 130px;
   transform: rotate(-10deg);
   animation: ${walk} 3s linear infinite 0.5s;
+
+  ${media.small} {
+    display: none;
+  }
+
+  ${media.middle} {
+    left: 270px;
+    top: 170px;
+  }
 `
 
 const Paw4 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 575px;
   top: 280px;
   transform: rotate(-20deg);
   animation: ${walk} 3s linear infinite 0.75s;
+
+  ${media.middle} {
+    left: 330px;
+    top: 10px;
+  }
 `
 
 const Paw5 = styled.div`
-  opacity: 0;
-  position: absolute;
-  left: 725px;
+  ${basePaw};
+  left: 650px;
   top: 50px;
   transform: rotate(10deg);
   animation: ${walk} 3s linear infinite 1s;
+
+  ${media.middle} {
+    display: none;
+  }
 `
 
 const Paw6 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 875px;
   top: 200px;
   transform: rotate(10deg);
   animation: ${walk} 3s linear infinite 1.25s;
+
+  ${media.large} {
+    display: none;
+  }
 `
 
 const Paw7 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 1075px;
   top: 50px;
   transform: rotate(20deg);
   animation: ${walk} 3s linear infinite 1.5s;
+
+  ${media.xl} {
+    display: none;
+  }
 `
 
 const Paw8 = styled.div`
-  opacity: 0;
-  position: absolute;
+  ${basePaw};
   left: 1150px;
   top: 250px;
   transform: rotate(10deg);
   animation: ${walk} 3s linear infinite 1.75s;
+
+  ${media.xl} {
+    display: none;
+  }
 `
 
 export default Paws
