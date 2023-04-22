589f748f7af5900cc86de1861a560fea0daeffb0
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-04-23 01:36:13 +0300

    fix registrationpage

--- packages/client/src/modules/registration/Registration.tsx
+++ packages/client/src/modules/registration/Registration.tsx
@@ -2,13 +2,27 @@ import RegistrationForm from '../../components/RegistrationForm'
 import ContrastingWrapper from '../../components/ContrastingWrapper'
 import { registration } from '../../store/auth/AuthActions'
 import { H3 } from '../../assets/styles/texts'
-import { useAppDispatch } from '../../hooks/store'
+import { useAppDispatch, useAppSelector } from '../../hooks/store'
 import { TSignUpFormValues } from '../../models/RegistrationModel'
+import { authSelectors } from '../../store/auth/AuthSelectors'
+import { useNavigate } from 'react-router'
+import { useEffect } from 'react'
+import { Routes } from '../../constants/routes'
 
 const Registration = () => {
   const dispatch = useAppDispatch()
+
+  //TODO вынести логику в хук или НОС (переход на протектид роуты)
+  const isAuth = useAppSelector(authSelectors.getIsAuth)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuth) navigate(Routes.HOME)
+  }, [isAuth])
+
   const handleSubmit = async (data: TSignUpFormValues) => {
     dispatch(registration(data))
+
   }
 
   return (
