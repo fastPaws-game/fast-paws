e1e8ea6f524ddc6f9636568e19338fcb0cbf43a7
mrHoft <ru.hoft@gmail.com>
2023-04-25 18:37:04 +0300

    SRR + HMR

---
+++ packages/client/ssr/App.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+
+function App() {
+  useEffect(() => {
+    const fetchServerData = async () => {
+      const url = `http://localhost:${__SERVER_PORT__}/api`
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchServerData()
+  }, [])
+  return <div className="App">Hello world!!!</div>
+}
+
+export default App
---
+++ packages/client/ssr/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// import { startServiceWorker } from '../src/utils/startServiceWorker.mjs'
+import { store } from '../src/store'
+import { Provider } from 'react-redux'
+
+// const store = createStore(window.__REDUX_STATE__)
+// delete window.__REDUX_STATE__
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
+// if (import.meta.env.PROD) startServiceWorker()
