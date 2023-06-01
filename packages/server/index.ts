48a6d410e41b5767ca18b9e040e55ff7bc656d75
Viktoria <115109501+ElPastel@users.noreply.github.com>
2023-06-01 12:00:45 +0400

    fix oauth (#84)
    * fix oauth
    
    * db-init

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
 import { UserAPIRepository, UserRepository } from './src/repository/UserAPI'
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { proxy } from './src/middlewares/proxy'
 import topicsRouter from './src/routes/topics'
@@ -12,11 +12,13 @@ import commentsRouter from './src/routes/comments'
 import { dbConnect } from './db'
 import { API_VERSION } from './src/constants'
 
-const PORT = Number(process.env.SERVER_PORT) || 3001
+dotenv.config({ path: '../../.env' })
+
+const PORT = Number(process.env.SERVER_PORT) || 5000
 const isDev = process.env.NODE_ENV === 'development'
 
 async function startServer() {
-  dbConnect()
+  await dbConnect()
   const app = express()
 
   app.use(
