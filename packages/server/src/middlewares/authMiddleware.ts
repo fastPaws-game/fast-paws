fff77c5549960d16a67cad4ffbc2fd23df7ea0e3
Victoria Yakovleva <v.yakovleva@inostudio.net>
2023-06-12 02:12:37 +0300

    fix check errors

--- packages/server/src/middlewares/authMiddleware.ts
+++ packages/server/src/middlewares/authMiddleware.ts
@@ -16,7 +16,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
   } catch (e) {
     //ручка стандартного API https://ya-praktikum.tech/api/v2/auth/user не возвращает AxiosError, кроме того объект ошибки не содержит code, statusCode и т.д. Поэтому приходится ориентироваться именно на строку ошибки
     if (e instanceof Error && e.message === 'Cookie is not valid') {
-      res.status(401).json({ message: 'Пользователь не авторизированcccccccccccccc' })
+      res.status(401).json({ message: 'Пользователь не авторизирован' })
     }
     if (!res.headersSent) {
       res.status(500).json({ message: 'Ошибка сервера' })
